b18da56d517605fa4fdbf9e1f4eaaf5a502fef3b
Josh Betz <j@joshbetz.com>
2012-06-20 13:15:51 -0500

    Show the pointer cursor for the add more users button

--- js/ajax-user-box.js
+++ js/ajax-user-box.js
@@ -1,5 +1,5 @@
 (function($){
-	$('#new-user-and-email').prepend("<a>+ Add more users</a>");
+	$('#new-user-and-email').prepend("<a style='cursor:pointer'>+ Add more users</a>");
 	$('#new-user-and-email a').click(function(i){
 		$('#new-user-and-email').append(function(){
 			return $('#new-user-and-email div:first').clone();
