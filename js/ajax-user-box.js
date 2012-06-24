03492516656512bc3f198e0a3d404b1f02eb870c
Josh Betz <j@joshbetz.com>
2012-06-24 15:11:17 -0500

    Remove "+" symbol from "Add more users" button.

--- js/ajax-user-box.js
+++ js/ajax-user-box.js
@@ -1,5 +1,5 @@
 (function($){
-	$('#col-left h3').append("<a class='button add-new'>+ Add more users</a>");
+	$('#col-left h3').append("<a class='button add-new'>Add more users</a>");
 	$('.add-new.button').click(function(i){
 		$('#new-user-and-email').append(function(){
 			return $('#new-user-and-email .row:first').clone().show();
