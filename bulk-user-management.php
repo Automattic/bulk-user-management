9f1ecfcb042a40ab41f8e79d05977368081ef3f4
Josh Betz <j@joshbetz.com>
2012-06-25 20:29:01 -0500

    Add `$cap` parameter to `get_blog_ids()` to enable flexibility for checking caps depending on the situation i.e.) Only list site in the remove form if the current user can actually remove users from those sites

--- bulk-user-management.php
+++ bulk-user-management.php
@@ -248,7 +248,7 @@ class Bulk_User_Management {
 					<ul class="site-checklist">
 						<?php
 							$bulk_users_table = new Bulk_User_Table();
-							$blogs = $bulk_users_table->get_blog_ids();
+							$blogs = $bulk_users_table->get_blog_ids( 'add_users' );
 
 							foreach ( $blogs as $id ) {
 								$blog = get_blog_details($id);
@@ -343,7 +343,7 @@ class Bulk_User_Management {
 		// Make sure the current user can create users on all target blogs
 		foreach ( $blogids as $blog ) {
 			if ( ! current_user_can_for_blog( $blog, 'create_users') ) {
-				$error = new WP_Error( __( 'Cheatin&#8217; uh?', 'bulk-user-management' ) );
+				$error = new WP_Error( __( "Cheatin' huh?", 'bulk-user-management' ) );
 				wp_die( $error->get_error_message() );
 			}
 		}
